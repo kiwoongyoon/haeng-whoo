800428516a2e29aee08ad8b9a301ed8a19d5983e
Borieya0619 <borieya0619@gmail.com>
2023-02-05 16:20:06 +0900

    게시판 페이지 옵션 추가

--- client/src/pages/article/article.js
+++ client/src/pages/article/article.js
@@ -8,6 +8,8 @@ const data = {
         content:'아무 글',
         date:'2023-01-14',
         count: 33,
+        country: '영국',
+        tag: '휴양지',
     },
     2:{
         number:2,
@@ -16,6 +18,8 @@ const data = {
         content:'아무 글',
         date:'2023-01-15',
         count: 31,
+        country: '일본',
+        tag: '유적지',
     },
 };
 
@@ -24,17 +28,17 @@ const Article = ()=>{
     const params = useParams();//url파라미터를 useParams Hook으로 조회한다.
     const read =data[params.id]; 
     return(
-        <div class = "board_wrap">
-        <div class="board_title">
+        <div className = "board_wrap">
+        <div className="board_title">
             <strong>여행 후기</strong>
             <p>행후에서 당신의 여행 이야기를 작성해보세요!</p>
         </div>
-        <div class="board_view_wrap">
-            <div class="board_view">
-                <div class="title">
+        <div className="board_view_wrap">
+            <div className="board_view">
+                <div className="title">
                     {read.title}
                 </div>
-                <div class="info">
+                <div className="info">
                     <dl>
                         <dt>번호</dt>
                         <dd>{read.number}</dd>
@@ -51,13 +55,21 @@ const Article = ()=>{
                         <dt>조회</dt>
                         <dd>{read.count}</dd>
                     </dl>
+                    <dl>
+                        <dt>여행지</dt>
+                        <dd>{read.country}</dd>
+                    </dl>
+                    <dl>
+                        <dt>태그</dt>
+                        <dd>{read.tag}</dd>
+                    </dl>
                 </div>
-                <div class="content">
+                <div className="content">
                     {read.content}
                 </div>
             </div>
-            <div class="button_wrap">
-                <a href="/articles" class="on">목록</a>
+            <div className="button_wrap">
+                <a href="/articles" className="on">목록</a>
                 <a href="/edit">수정</a>
             </div>
         </div>
--- client/src/pages/articles/articles.js
+++ client/src/pages/articles/articles.js
@@ -2,64 +2,94 @@ import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import board_content from "../../data/board_content.json";
 import './articles.css';
+import Option from "../../contexts/option";
 
 const Articles = ()=>{  
     return(
-        <div class = "board_wrap">
-        <div class="board_title">
-            <div class="button_wrap">
-                <a href="/write" class="on">작성하기</a>
-                <a href="/" class="on">집으로</a>
+        <div className = "board_wrap">
+        <div className="board_title">
+            <div className = "options">
+                <div className = "country_option">
+                <select>
+                    <Option.Consumer>
+                        {
+                            value=>(value.countryList)
+                        }
+                    </Option.Consumer>
+                </select>
+                </div>
+                <div className = "sorting_option">
+                <select>
+                    <Option.Consumer>
+                        {
+                            value=>(value.sortingList)
+                        }
+                    </Option.Consumer>
+                </select>
+                </div>
+                <div className = "tag_option">
+                <select>
+                    <Option.Consumer>
+                        {
+                            value=>(value.tagList)
+                        }
+                    </Option.Consumer>
+                </select>
+                </div>
+            </div>
+            <div className="button_wrap">
+                <a href="/write" className="on">작성하기</a>
+                <a href="/" className="on">집으로</a>
             </div>
             <strong>여행 후기</strong>
             <p>행후에서 당신의 여행 이야기를 작성해보세요!</p>
         </div>
-        <div class="board_list_wrap">
-            <div class="board_list">
-                <div class="top">
-                    <div class="num">번호</div>
-                    <div class="title">제목</div>
-                    <div class="writer">글쓴이</div>
-                    <div class="date">작성일</div>
-                    <div class="count">조회</div>
+        <div className="board_list_wrap">
+            <div className="board_list">
+                <div className="top">
+                    <div className="num">번호</div>
+                    <div className="title">제목</div>
+                    <div className="writer">글쓴이</div>
+                    <div className="date">작성일</div>
+                    <div className="count">조회</div>
                 </div>
                 <div>
-                    <div class="num">2</div>
-                    <div class="title"><a href ="articles/2">글 제목</a></div>
-                    <div class="writer">홍길동</div>
-                    <div class="date">2023.01.15</div>
-                    <div class="count">31</div>
+                    <div className="num">2</div>
+                    <div className="title"><a href ="articles/2">글 제목</a></div>
+                    <div className="writer">홍길동</div>
+                    <div className="date">2023.01.15</div>
+                    <div className="count">31</div>
                 </div>
                 <div>
-                    <div class="num">1</div>
-                    <div class="title"><a href ="articles/1">글 제목1</a></div>
-                    <div class="writer">박효리</div>
-                    <div class="date">2023.01.14</div>
-                    <div class="count">33</div>
+                    <div className="num">1</div>
+                    <div className="title"><a href ="articles/1">글 제목1</a></div>
+                    <div className="writer">박효리</div>
+                    <div className="date">2023.01.14</div>
+                    <div className="count">33</div>
                 </div>
                 {board_content.map((content)=>(
                     <div>
-                        <div class="num">{content["number"]}</div>
-                        <div class="title"><Link to={""+content["number"]}>{content["title"]}</Link></div>
-                        <div class="writer">{content["author"]}</div>
-                        <div class="date">{content["date"]}</div>
-                        <div class="count">{content["count"]}</div>
+                        <div className="num">{content["number"]}</div>
+                        <div className="title"><Link to={""+content["number"]}>{content["title"]}</Link></div>
+                        <div className="writer">{content["author"]}</div>
+                        <div className="date">{content["date"]}</div>
+                        <div className="count">{content["count"]}</div>
                     </div>
                 ))}
             </div>
-            <div class="board_page">
-                <a href="#" class="button first">&gt;&gt;</a>
-                <a href="#" class="button prev">&gt;</a>
-                <a href="#" class = "num on">1</a>
-                <a href="#" class = "num">2</a>
-                <a href="#" class = "num">3</a>
-                <a href="#" class = "num">4</a>
-                <a href="#" class = "num">5</a>
-                <a href="#" class="button last">&lt;&lt;</a>
-                <a href="#" class="button next">&lt;</a>
+            <div className="board_page">
+                <a href="#" className="button first">&gt;&gt;</a>
+                <a href="#" className="button prev">&gt;</a>
+                <a href="#" className = "num on">1</a>
+                <a href="#" className = "num">2</a>
+                <a href="#" className = "num">3</a>
+                <a href="#" className = "num">4</a>
+                <a href="#" className = "num">5</a>
+                <a href="#" className="button last">&lt;&lt;</a>
+                <a href="#" className="button next">&lt;</a>
             </div>
         </div>
-        <div class="search_field">
+        <div className="search_field">
             <input type="text" placeholder="제목으로 검색해보세요."></input> 
             <button>검색</button>
         </div>
